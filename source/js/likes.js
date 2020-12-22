87091dc8728ca5c1877d84b814bede498656560c
Иван <kleimenov416@gmail.com>
2020-12-22 12:41:21 -0500

    start adding JS code, add likes counter feature

--- source/js/likes.js
+++ source/js/likes.js
@@ -1,16 +1,14 @@
 let thumb = document.querySelectorAll('.photo-list__likes', ':after')
 let likesNumber = document.querySelectorAll('.photo-list__like-count')
 
-/*
-thumb.onclick = () => {
-    likesNumber.textContent = cnt;
-}
-*/
+
+
 
 for (let index in thumb) {
     let likesCounter = 0;
     thumb[index].onclick = () => {
         likesCounter++;
         likesNumber[index].textContent = likesCounter;
+        thumb[index].classList.toggle('added');
     }
 }
