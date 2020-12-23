a96dd4d19046e2298132eade9a47a050d93792e7
Иван <kleimenov416@gmail.com>
2020-12-23 11:38:13 -0500

    start working at modal on the index page

--- source/js/likes.js
+++ source/js/likes.js
@@ -4,12 +4,12 @@ let likesNumber = document.querySelectorAll('.photo-list__like-count')
 
 
 for (let index in thumb) {
-    thumb[index].onclick = () => {
-        if (thumb[index].classList.contains('added')) {
-            likesNumber[index].textContent--;
-        } else {
-            likesNumber[index].textContent++;
-        }
-        thumb[index].classList.toggle('added');
+  thumb[index].onclick = () => {
+    if (thumb[index].classList.contains('added')) {
+        likesNumber[index].textContent--;
+    } else {
+        likesNumber[index].textContent++;
     }
+    thumb[index].classList.toggle('added');
+  }
 }
