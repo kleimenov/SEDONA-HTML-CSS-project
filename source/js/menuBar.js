7d11db02526c0e4af72a98b9928f1e38f6cd1348
Иван <kleimenov416@gmail.com>
2020-12-28 08:56:21 -0500

    start working at nab bar

--- source/js/menuBar.js
+++ source/js/menuBar.js
@@ -1,15 +1,15 @@
 let menuBar = document.querySelector('.main-nav__toggle');
 let navBarPopUp = document.querySelector('.site-list');
 let navList = document.querySelector('.main-nav__list');
-
+let navLogo = document.querySelector('.page-header__logo-image');
 
 if(window.innerWidth < 530) {
-    navBarPopUp.classList.add('nav--hide')
+    navBarPopUp.classList.add('nav--hide');
 }
 
 
 menuBar.addEventListener('click', (evt)=> {
     evt.preventDefault();
     navBarPopUp.classList.toggle('nav--hide')
-
+    navLogo.classList.toggle('nav--hide')
 })
