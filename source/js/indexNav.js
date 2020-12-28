f0b7fbf46b907468aaf58f691382cef98da2d10f
Иван <kleimenov416@gmail.com>
2020-12-28 10:42:22 -0500

    add couple js files: indexNav,js , photoNav.js , formNav.js and implement nav bar logic, add toggle button

--- source/js/indexNav.js
+++ source/js/indexNav.js
@@ -4,14 +4,12 @@ let navList = document.querySelector('.main-nav__list');
 let navLogo = document.querySelector('.page-header__logo-image');
 
 
+
 if(window.innerWidth < 530) {
     navBarPopUp.classList.add('nav--hide');
 }
-
-
 menuBar.addEventListener('click', (evt)=> {
     evt.preventDefault();
     navBarPopUp.classList.toggle('nav--hide');
     navLogo.classList.toggle('nav--hide');
-    
 })
