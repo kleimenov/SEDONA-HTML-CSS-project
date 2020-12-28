f8836fde4d80d603edd24dce3786d382c8b9210a
Иван <kleimenov416@gmail.com>
2020-12-28 08:05:52 -0500

    start working at nab bar

--- source/js/menuBar.js
+++ source/js/menuBar.js
@@ -1,6 +1,8 @@
 let menuBar = document.querySelector('.main-nav__toggle');
 let popUp = document.querySelector('.site-list')
 
+popUp.classList.add('hide-nav')
+
 menuBar.addEventListener('click', (evt)=> {
     evt.preventDefault();
     console.log('Hello')
